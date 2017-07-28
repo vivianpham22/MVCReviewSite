1ecc5923874b82254deedcc86fd122b4368a31e5
kingveee <vivianpham22@gmail.com>
2017-07-28 15:26:28 -0400

    styling

---
+++ MVCReviewSiteOG/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteOG.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Location { get; set; }
+        public DateTime Published { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
